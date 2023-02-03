94612a8313ccd742a9c2f442dfc0a68dc8646f9d
LuisPerdomo123 <leperdomoo@correo.udistrital.edu.co>
2023-02-03 16:10:32 -0500

    Uso de la palabra final

---
+++ PalabraFinal/src/domain/Empleado.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+/**
+ *
+ * @author lepo9
+ */
+public class Empleado //extends Persona 
+{
+    
+//    /*
+//    La palabra Override permite realizar sobre escritura del metodo de la clase padre..
+//    Esto se conoce como sobreescritura
+//    */
+//    
+//    @Override
+//    public final void imprimir(){
+//    
+//        System.out.println("Metodo imprimir desde clase hija");
+//    }
+    
+    
+    
+}
