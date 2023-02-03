69a80b00f47d5324468f15576863ef6e451b88f2
LuisPerdomo123 <leperdomoo@correo.udistrital.edu.co>
2023-02-03 16:10:18 -0500

    Uso de la palabra final

---
+++ PalabraFinal/src/domain/Persona.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+/**
+ *
+ * @author lepo9
+ */
+public class Persona {
+    
+    /*
+    Cuando se combina la palabra final y static se conoce como constante.
+    Final: No permite modificar el contenido.
+    Static: Realizar el llamado de la variable sin crear objeto.
+    Se recomuenda que la constante de defina en maysucular y guion bajo para separar palabras
+    */
+    
+    public final static int MI_CONSTANTE = 1;
+    
+    /*
+    Si se incluya la palabra final en la clase o en el metodo, las clases
+    hijas no podran modificar su contenido.
+    */
+    
+    public void imprimir(){
+    
+        System.out.println("Metodo imprimir");
+    }
+}
